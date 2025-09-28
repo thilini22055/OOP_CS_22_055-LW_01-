bb16cf49634b269b4b50750d5fd77e83bd8b826d
thilini22055 <thiliniyashoda10@gmail.com>
2025-09-28 19:37:09 +0530

    Update Q5_LW_02.java

--- Q5_LW_02.java
+++ Q5_LW_02.java
@@ -5,23 +5,23 @@ public class Q5_LW_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Input sentence
+        
         System.out.print("Enter a sentence: ");
         String sentence = scanner.nextLine();
 
-        // Tokenize sentence into words (split by space)
+        
         StringTokenizer tokenizer = new StringTokenizer(sentence, " ");
 
-        // Build a cleaned version: only lowercase letters/numbers, no punctuation
+       
         StringBuilder cleaned = new StringBuilder();
         while (tokenizer.hasMoreTokens()) {
             String word = tokenizer.nextToken().toLowerCase();
-            // Remove punctuation characters from each word
+           
             word = word.replaceAll("[^a-z0-9]", "");
             cleaned.append(word);
         }
 
-        // Check palindrome
+       
         String cleanedStr = cleaned.toString();
         String reversedStr = cleaned.reverse().toString();
 
@@ -34,3 +34,4 @@ public class Q5_LW_02 {
         scanner.close();
     }
 }
+
