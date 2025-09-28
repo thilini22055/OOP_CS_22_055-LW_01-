fdff9fbc7d8e812cad7bf5cec7f196e334127dd6
thilini22055 <thiliniyashoda10@gmail.com>
2025-09-28 19:18:49 +0530

    Add files via upload

---
+++ Q2_LW_02.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Q2_LW_02 {
+
+    // Method to return the number of digits in an integer
+    public static int countDigits(int number) {
+        // Handle 0 specially, since log10(0) is undefined
+        if (number == 0) {
+            return 1;
+        }
+
+        int count = 0;
+        while (number != 0) {
+            number /= 10;
+            count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Enter an integer (negative number to stop): ");
+            int input = scanner.nextInt();
+
+            if (input < 0) {
+                System.out.println("Program terminated.");
+                break;
+            }
+
+            int digits = countDigits(input);
+            System.out.println(input + " has " + digits + " digit(s).");
+        }
+
+        scanner.close();
+    }
+}
